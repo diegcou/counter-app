85bfe091bede427129e15fb2745a90ea6ec8d4f0
Diego Coutinho <coutinhoarias@gmail.com>
2021-10-28 22:09:32 -0400

    Added "Reset All" button.

--- Counter.js
+++ Counter.js
@@ -1,5 +1,6 @@
 let count = 0
 let countNumber = document.getElementById("count-number")
+let saveNumber = document.getElementById("last-count")
 
 function increment() {
     count = count + 1
@@ -9,4 +10,21 @@ function increment() {
 function decrement() {
     count = count - 1
     countNumber.innerText = count
+}
+
+function reset() {
+    count = 0
+    countNumber.innerText = 0
+}
+
+function save() {
+    saveNumber.innerText = count
+    count = 0
+    countNumber.innerText = 0
+}
+
+function resetall() {
+    countNumber.innerText = 0
+    saveNumber.innerText = 0
+    count = 0
 }
